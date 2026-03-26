1779c5d34087144f2ecbe4d428150a954d643c1c
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-03-26 08:38:19 -0300

    Atualiza comentários

--- Mês 03 - Março/25.03.2026/Ex5.java
+++ Mês 03 - Março/25.03.2026/Ex5.java
@@ -6,11 +6,10 @@ public class Ex5 {
 
 		Scanner input = new Scanner(System.in);
 
-		// A variável somaAltura precisa ser declarada antes do for porque ela será
-		// usada
-		// para acumular os valores digitados ao longo de todas as iterações do laço.
-		// Se fosse declarada dentro do for, seria reinicializada a cada repetição,
-		// impedindo o cálculo correto da soma total.
+		// somaAltura precisa ser declarada antes do for
+		// Será usada para acumular os valores digitados ao longo das iterações
+		// Se fosse declarada dentro do for, seria reinicializada a cada repetição
+		// Impedindo o cálculo correto da soma total.
 		double somaAltura = 0;
 
 		// Estrutura de repetição que executa exatamente 5 vezes (i de 0 até 4).
@@ -18,14 +17,12 @@ public class Ex5 {
 			System.out.println("Insira a altura");
 			double altura = input.nextDouble();
 
-			somaAltura += altura;
+			somaAltura += altura; // somaAltura = somaAltura + altura
 		}
 
-		// A variável media deve ser calculada após o término do for, pois somente nesse
-		// ponto
-		// a variável somaAltura contém a soma completa de todas as alturas.
+		// A variável media deve ser calculada após o término do for
+		// Pois somente nesse ponto a variável somaAltura contém a soma completa
 		// Se fosse calculada dentro do laço, o valor ainda estaria incompleto.
-		// Atualização do GitHub
 		double media = somaAltura / 5;
 
 		System.out.println("Média de altura " + media + " metros");
