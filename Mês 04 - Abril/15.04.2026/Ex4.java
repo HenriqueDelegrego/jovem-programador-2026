1123fe5a67bf0195423fa04ffc10b37d43a0500d
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-04-15 11:17:53 -0300

    Remove comentários não necessários

--- Mês 04 - Abril/15.04.2026/Ex4.java
+++ Mês 04 - Abril/15.04.2026/Ex4.java
@@ -19,19 +19,15 @@ public class Ex4 {
 	 */
 	public static void obterMenorValor(int vetor[]) {
 
-		// Começamos com o maior valor possível de um inteiro
 		int menorValor = Integer.MAX_VALUE;
 
-		// Percorremos todo o vetor
 		for (int i = 0; i < vetor.length; i++) {
 
-			// Se encontrarmos um valor menor, atualizamos
 			if (vetor[i] < menorValor) {
 				menorValor = vetor[i];
 			}
 		}
 
-		// Exibe o resultado
 		System.out.println("O menor valor é: " + menorValor);
 
 	}
@@ -43,19 +39,15 @@ public class Ex4 {
 	 */
 	public static void obterMaiorValor(int vetor[]) {
 
-		// Começamos com o menor valor possível de um inteiro
 		int maiorValor = Integer.MIN_VALUE;
 
-		// Percorremos todo o vetor
 		for (int i = 0; i < vetor.length; i++) {
 
-			// Se encontrarmos um valor maior, atualizamos
 			if (vetor[i] > maiorValor) {
 				maiorValor = vetor[i];
 			}
 		}
 
-		// Exibe o resultado
 		System.out.println("O maior valor é: " + maiorValor);
 
 	}
@@ -67,18 +59,14 @@ public class Ex4 {
 	 */
 	public static void obterMedia(int vetor[]) {
 
-		// Variável para acumular a soma dos valores
 		double soma = 0;
 
-		// Percorremos o vetor somando cada elemento
 		for (int i = 0; i < vetor.length; i++) {
 			soma += vetor[i];
 		}
 
-		// Calculamos a média
 		double media = soma / vetor.length;
 
-		// Exibe o resultado
 		System.out.println("A média é: " + media);
 
 	}
