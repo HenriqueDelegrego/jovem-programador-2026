d2cbe82b374ef82fc62144678ebdbb2ff3fe3846
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-04-16 11:26:23 -0300

    Adiciona Ex9

---
+++ Mês 04 - Abril/16.04.2026/Ex9.java
@@ -0,0 +1,24 @@
+
+public class Ex9 {
+
+	public static void main(String[] args) {
+
+		System.out.println(determinarParOuImpar(10));
+
+	}
+
+	/**
+	 * Verifica se um número inteiro é par.
+	 *
+	 * @param numero número a ser verificado
+	 * @return true se o número for par, false caso contrário
+	 */
+	public static boolean determinarParOuImpar(int numero) {
+
+		if (numero % 2 == 0) {
+			return true;
+		}
+		return false;
+	}
+
+}
