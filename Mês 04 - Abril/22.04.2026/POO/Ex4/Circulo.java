8910db585c8a19c3edbf4a3a8aa38e4a38e29c2f
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-04-22 11:31:13 -0300

    Adiciona Ex4

---
+++ Mês 04 - Abril/22.04.2026/POO/Ex4/Circulo.java
@@ -0,0 +1,15 @@
+
+public class Circulo {
+
+	double raio;
+
+	/**
+	 * Calcula a área do círculo com base no raio.
+	 *
+	 * @return a área do círculo
+	 */
+	public double obterArea() {
+		return Math.PI * Math.pow(raio, 2);
+	}
+
+}
---
+++ Mês 04 - Abril/22.04.2026/POO/Ex4/Ex4.java
@@ -0,0 +1,13 @@
+
+public class Ex4 {
+	
+	public static void main(String[] args) {
+		
+		Circulo c1 = new Circulo();
+		c1.raio = 2;
+		
+		System.out.println(c1.obterArea());
+		
+	}
+
+}
