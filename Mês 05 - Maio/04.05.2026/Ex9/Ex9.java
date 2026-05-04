01d63c17dea70174a03376de4323d4e21837056e
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-05-04 11:28:51 -0300

    Adiciona Ex9

---
+++ Mês 05 - Maio/04.05.2026/Ex9/Ex9.java
@@ -0,0 +1,23 @@
+
+public class Ex9 {
+
+	public static void main(String[] args) {
+
+		Carro c1 = new Carro(5);
+		System.out.println(c1);
+
+		c1.acelerar(15);
+		System.out.println(c1);
+
+		c1.reduzir(5);
+		System.out.println(c1);
+
+		c1.reduzir(10);
+		System.out.println(c1);
+
+		c1.reduzir(10);
+		System.out.println(c1);
+
+	}
+
+}
